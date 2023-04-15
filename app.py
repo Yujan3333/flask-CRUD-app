133b1edb7b17d9012c9ad039b1a3da53b74839e4
Yujan3333 <yujanbasnet18@gmail.com>
2023-04-15 16:17:21 +0545

    5Handling Form Errors

--- app.py
+++ app.py
@@ -29,14 +29,18 @@ def form():
     last_name= request.form.get("last_name")
     email= request.form.get("email")
     
+    if not first_name or not last_name or not email:
+        error_statement = "All fields must be filled !!"
+        return render_template('subscribe.html', error_statement=error_statement, first_name=first_name,last_name=last_name,email=email)
+    
     subscribers.append(f"Name:{first_name}{last_name} || Email: {email}")
     
     # using jinja
-    title="form page"
+    title="Display Details"
     
     # return render_template("form.html",title=title,first_name=first_name,last_name=last_name,email=email)
     
-    return render_template("form.html",title=title,subscribers=subscribers)
+    return render_template("form1.html",title=title,subscribers=subscribers)
 
 
 
