afa76269158c19e2e4df3ee5a69574c7c3e480b4
Yujan3333 <yujanbasnet18@gmail.com>
2023-04-19 18:16:16 +0545

    8 friends database insert and show

--- app.py
+++ app.py
@@ -1,22 +1,80 @@
-from flask import Flask, render_template, request
-# This library allows us to use smtp
-import smtplib
+from flask import Flask, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from sqlalchemy import desc
 
-app= Flask(__name__);
+
+app= Flask(__name__)
+# '///' represents the relative path right here
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///friends.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+# Initialize the database
+db=SQLAlchemy(app)
+
+# For runnind command in terminal
+app.app_context().push()
+
+
+# Create db model
+#write python that is converted to SQL
+class Friends(db.Model):
+    id= db.Column(db.Integer ,primary_key=True)
+    name= db.Column(db.String(100), nullable=False)
+    date_created= db.Column(db.DateTime, default=datetime.utcnow)
+    
+    # Create a function to return a string whe we add something
+    def __repr__(self):
+        return '<Name %r>' %self.id
+
+
 
 #For the displaying of the content of the form
 subscribers = [];
+
+
+@app.route('/friends', methods=['POST','GET'])
+def friends():
+    title="Friends of Yujan"
+    
+    if request.method =='POST':
+        friend_name= request.form['name']
+        # adding the friend name from form to db table Friends(Class dec above)
+        new_friend=  Friends(name=friend_name)
+        
+        # push to db
+        try:
+            db.session.add(new_friend)
+            db.session.commit()
+            return redirect('/friends')
+        except:
+            return "There was error adding friends name to db"
+    else:
+        friends =Friends.query.order_by(desc(Friends.date_created))
+        return render_template("friends.html",title=title, friends=friends)
+
+
+
+
 
 # Homepage routing
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
+
+
 @app.route('/about')
 def about():
     # using jinja
     title="About Yujan"
     return render_template("about.html",title=title)
+
+
+
+
 
 @app.route('/subscribe')
 def subscribe():
